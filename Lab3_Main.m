0edecc8c4ccba1b185541004af4b6baf6d239d5f
Jwzientek <jawz2406@colorado.edu>
2026-03-31 12:30:12 -0600

    starting values

--- Lab3_Main.m
+++ Lab3_Main.m
@@ -12,4 +12,17 @@ clc
 clear
 close all
 
-%% 
+%% Starting Values
+NACA = 0021;
+
+% Ensure NACA is treated as a 4-digit code with leading zeros preserved
+NACA_string = sprintf('%04d', NACA);
+
+% Calculate the maximum camber 
+m = str2double(NACA_string(1)) / 100;
+
+% Calculate the location of maximum camber 
+p = str2double(NACA_string(2)) / 10;
+
+% Calculate the thickness ratio 
+t = str2double(NACA_string(3:4)) / 100;
