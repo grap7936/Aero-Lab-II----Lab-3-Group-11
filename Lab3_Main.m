1c76e2b54b2a26f68a78cc12882eca89e5b066a3
grap7936 <grap7936@colorado.edu>
2026-03-31 12:18:15 -0600

    Add files via upload

---
+++ Lab3_Main.m
@@ -0,0 +1,15 @@
+%% ASEN 3802 -- Lab 3 -- Aerodynamics Lab
+%% Summary: We will model a 4 digit NACA airfoil both numerically and with an equi-angular representation and then we will use 
+
+
+
+
+
+
+
+%% Housekeeping
+clc
+clear
+close all
+
+%% 
