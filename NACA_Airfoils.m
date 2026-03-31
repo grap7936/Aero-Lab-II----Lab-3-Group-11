bfca684bb83c1109f0c3841a29df47496e99ec36
grap7936 <grap7936@colorado.edu>
2026-03-31 11:57:13 -0600

    Add files via upload

---
+++ NACA_Airfoils.m
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+function [x_b,y_b] =  NACA_Airfoils(m,p,t,c,N)
+
+% Goal/Purpose: Modeling a 4 digit NACA airfoil representation numerically by converting 4 digit
+% inputs into relevant variables and finding discrete x and y coordinates for the top and bottom of
+% the airfoil using numerically derived equations
+
+% Inputs:
+% 1.) m = max airfoil camber. This is the 1st digit (leftmost) of the 4 digit NACA airfoil
+% representation and represents the max camber 
+
+% Outputs: 
+
+
+
+
+
+
+
+end
