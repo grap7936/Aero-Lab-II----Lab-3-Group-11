ac816ad5c9e8b4fcb4d17a69cc84a335403712fe
mcco8549 <mcco8549@colorado.edu>
2026-04-07 18:47:23 -0600

    merged task one and two

--- Lab3_Main.m
+++ Lab3_Main.m
@@ -20,30 +20,77 @@ clc
 clear
 close all
 
-%% Task 1 -- Numerical Representation of NAVA 4 digit airfoil
+%% Task 1 -- Plot of NACA 0021 and NACA 2421 using 50 panels
+% NACA 0021
+m = 0/100;
+p = 0/10;
+t = 21/100;
+% NACA 2421
+m2 = 4/100;
+p2 = 4/10;
+t2 = 15/100;
 
+c = 1;
+N = 50;
+
+[x_b, y_b, x_c, y_c] =  NACA_Airfoils(m,p,t,c,N);
+[x_b2, y_b2, x_c2, y_c2] =  NACA_Airfoils(m2,p2,t2,c,N);
+
+figure();
+subplot(2, 1, 1);
+plot(x_b, y_b, "-xb", "LineWidth",1.5)
+hold on 
+plot(x_c, y_c, "--g", "LineWidth",1.5)
+axis equal % implement correcting dimensions so that MATLAB does not stretch the y-axis
+xlim([0 c]) % set correct graph dimensions
+ylim([-0.2*c 0.2*c])
+grid on
+xlabel('X - Coordinates');
+ylabel('Y - Coordinates');
+title("NACA 0012 Airfoil")
+legend('Airfoil', 'Camberline')
+
+subplot(2, 1, 2);
+plot(x_b2, y_b2, "-xr", "LineWidth",1.5)
+hold on;
+plot(x_c2, y_c2, "--g", "LineWidth",1.5)
+axis equal % implement correcting dimensions so that MATLAB does not stretch the y-axis
+xlim([0 c]) % set correct graph dimensions
+ylim([-0.2*c 0.2*c])
+grid on
+xlabel('X - Coordinates');
+ylabel('Y - Coordinates');
+title("NACA 2421 Airfoil");
+legend('Airfoil', 'Camberline')
+
+print('task1', '-dpng', '-r500');
+
+clc
+clear
+
+%% Task 2 -- 
 % Define test variables:
+i = 1;
 m = 0/100;
 p = 0/10;
 t = 21/100;
 c = 1;
 CL_old = 0;
 error = 1.5; 
-N = 1000;
+N_exact = 500;
 
 alpha = 12; % [deg]
 v_inf = 50; % [m/s]
 Numerical_Plot = 1; % variable to turn on numerical plots for vortex panel method
 
 % Call numerical function to get point distribution
-[x_b, y_b] =  NACA_Airfoils(m,p,t,c,N);
+[x_b, y_b] =  NACA_Airfoils(m,p,t,c,N_exact);
 
 
 % Call vortex panel method code to get output plot
 [CL_accurate] = Vortex_Panel(x_b, y_b, v_inf, alpha);
-
-N = 5; 
-while error > 1 
+N = 2; 
+while error > 1
 
 alpha = 12; % [deg]
 v_inf = 50; % [m/s]
@@ -54,27 +101,33 @@ Numerical_Plot = 1; % variable to turn on numerical plots for vortex panel metho
 
 
 % Call vortex panel method code to get output plot
-[CL] = Vortex_Panel(x_b, y_b, v_inf, alpha);
+[CL(i)] = Vortex_Panel(x_b, y_b, v_inf, alpha);
 
-error = abs((CL_accurate - CL)/(CL)) * 100;
-
+% error = abs((CL(i)- CL_accurate)/(CL_accurate)) * 100;
+error = abs((CL_accurate- CL(i))/(CL(i))) * 100;
 if error > 1
     N = N+1;
-    CL_old = CL;
-
+    CL_old = CL(i);
+    i = i + 1;
 end
 end
+N_vec = 1:length(CL);
+disp(['Number of total panels: ', num2str(2*(length(N_vec)))])
 
 figure();
-plot(x_b, y_b, "r", "LineWidth",1.5)
-axis equal % implement correcting dimensions so that MATLAB does not stretch the y-axis
-xlim([0 c]) % set correct graph dimensions
-ylim([-0.2*c 0.2*c])
-grid on
+plot(N_vec, CL,"LineWidth",1.5);
+xlabel('Number of Panels on Upper and Lower Surface');
+ylabel('Sectional Coefficient of Lift');
+title('Convergence of CL With Respect To Number of Panels')
+grid on 
 
-disp(['Number of total panels: ', num2str(2*N)])
+Results_Table = table( ...
+    N_exact, CL_accurate, ...
+    N_vec(end), CL(end), error(end), ...
+    'VariableNames', {'N_exact', 'CL_exact', 'N_predicted', 'CL_predicted', 'Percent_Error'});
+disp(Results_Table)
 
-
+%% Task 3
 % Define test variables:
 m = 2/100;
 p = 4/10;
